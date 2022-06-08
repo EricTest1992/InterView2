68daca684938c1a894884d3df84b4869bec3e6e9
eric.zhuang <chuang1892@gmail.com>
2022-06-08 13:04:06 +0800

    新增 ViewModel

---
+++ InterView/Models/ViewModels/EmployeeViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace InterView.Models.ViewModels
+{
+    public class EmployeeViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string Phone { get; set; } = null!;
+        public string Address { get; set; } = null!;
+    }
+}
