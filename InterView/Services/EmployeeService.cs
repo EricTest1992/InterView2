b339233af8fdd23ada17527ad15145146ffdcc93
eric.zhuang <chuang1892@gmail.com>
2022-06-08 19:05:41 +0800

    實作 Service 與介面

---
+++ InterView/Services/EmployeeService.cs
@@ -0,0 +1,33 @@
+﻿using InterView.Models.ViewModels;
+using InterView.Services.Interface;
+
+namespace InterView.Services
+{
+    public class EmployeeService : IEmployeeService
+    {
+        public void DeleteEmployees(out string errMsg)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<EmployeeViewModel> GetEmployees()
+        {
+            throw new NotImplementedException();
+        }
+
+        public EmployeeViewModel GetEmployeesSelect(int employeeID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void InsertEmployees(out string errMsg)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateEmployees(out string errMsg)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ InterView/Services/Interface/IEmployeeService.cs
@@ -0,0 +1,15 @@
+﻿using InterView.Models.ViewModels;
+
+namespace InterView.Services.Interface
+{
+    public interface IEmployeeService
+    {
+        public List<EmployeeViewModel> GetEmployees();
+
+        public EmployeeViewModel GetEmployeesSelect(int employeeID);
+
+        public void InsertEmployees(out string errMsg);
+        public void UpdateEmployees(out string errMsg);
+        public void DeleteEmployees(out string errMsg);
+    }
+}
