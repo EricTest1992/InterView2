75c2141322478afa93a83f400344d2a5179c7d2a
eric.zhuang <chuang1892@gmail.com>
2022-06-08 13:01:04 +0800

    新增 DB 連線

---
+++ InterView/Models/DAO/Employee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace InterView.Models.DAO
+{
+    public partial class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string Phone { get; set; } = null!;
+        public string Address { get; set; } = null!;
+    }
+}
